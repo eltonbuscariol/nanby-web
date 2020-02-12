caa3e690c55946221eec31aa35901335f114f00d
Elton <elton.buscariol@gmail.com>
2020-02-12 20:36:36 -0300

    Design Home

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import { Header, Footer } from '../Components/Layouts';
+import React from "react";
+import { Header } from "../Components/Layouts";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import ChildCareIcon from "@material-ui/icons/ChildCare";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Footer />
+      <Drawer variant="permanent">
+        <List>
+          <ListItem buttom>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+          <ListItem buttom>
+            <ListItemIcon>
+              <ChildCareIcon />
+            </ListItemIcon>
+            <ListItemText primary="Filhos" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <main>
+        <div className="root" />
+        <footer>
+          <p>Copyright @2020</p>
+        </footer>
+      </main>
     </>
   );
 }
--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,70 +1,79 @@
-import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import { APP_NAME } from '../../Constantes';
-import Button from '@material-ui/core/Button';
-import './styles.css';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import { login } from '../../Services/auth';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { APP_NAME } from "../../Constantes";
+import Button from "@material-ui/core/Button";
+import "./styles.css";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import { login } from "../../Services/auth";
+import { Typography, Grid, CssBaseline, Avatar } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Link from "@material-ui/core/Link";
 
 class SingIn extends Component {
   state = {
-    email: '',
-    password: '',
-    error: ''
+    email: "",
+    password: "",
+    error: ""
   };
 
   handleSignIn = async e => {
     e.preventDefault();
     const { email, password } = this.state;
     if (!email || !password) {
-      this.setState({ error: 'Preencha e-mail e senha para continuar!' });
+      this.setState({ error: "Preencha e-mail e senha para continuar!" });
     } else {
       login(email);
-      this.props.history.push('/app');
+      this.props.history.push("/app");
     }
   };
 
   render() {
     return (
-      <div className="Container">
-        <Paper className="paper">
-          <form onSubmit={this.handleSignIn}>
-            {this.state.error && <p>{this.state.error}</p>}
-            <h2 className="app-name">{APP_NAME.toUpperCase()}</h2>
+      <Grid container component="main" className="root">
+        <Grid item xs={false} sm={4} md={7} className="image" />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className="paper">
+            <Avatar className="avatar">
+              <LockOutlinedIcon />
+            </Avatar>
             <label className="title">Digite suas credenciais</label>
-            <TextField
-              type="email"
-              label="Endereço de email"
-              margin="normal"
-              variant="outlined"
-              fullWidth
-              autofocus
-              onChange={e => this.setState({ email: e.target.value })}
-            />
-            <TextField
-              type="password"
-              label="Senha"
-              variant="outlined"
-              fullWidth
-              onChange={e => this.setState({ password: e.target.value })}
-            />
-            <Button
-              type="submit"
-              className="submit-button"
-              variant="contained"
-              color="primary"
-              fullWidth
-            >
-              Entrar
-            </Button>
+            <form onSubmit={this.handleSignIn}>
+              {this.state.error && <p>{this.state.error}</p>}
+              <TextField
+                type="email"
+                label="Endereço de email"
+                margin="normal"
+                variant="outlined"
+                fullWidth
+                autofocus
+                onChange={e => this.setState({ email: e.target.value })}
+              />
+              <TextField
+                type="password"
+                label="Senha"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+              <Button
+                type="submit"
+                className="submit-button"
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                Entrar
+              </Button>
+            </form>
             <hr className="line-divisor" />
             <Link className="signup-link" to="/signup">
               Criar conta grátis
             </Link>
-          </form>
-        </Paper>
-      </div>
+          </div>
+        </Grid>
+      </Grid>
     );
   }
 }
